d990c97f7c1344b8e59bd4f21fff2952343eac77
leoabdou1122 <leoabdou1122@gmail.com>
2023-06-18 15:39:38 -0700

    print alphaber lowwercase and uppercase using putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,14 @@
 int main(void)
 {
 char a;
-for (a = 'A'; a <= 'Z'; a++)
+char b;
+for (a = 'a'; a <= 'z'; a++)
 {
 putchar(a);
+}
+for (b = 'A'; b <= 'Z'; b++)
+{
+putchar(b);
 }
 putchar('\n');
 return (0);
