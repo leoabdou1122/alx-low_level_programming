5b38a25efee8117a19816d62424b235669452a39
leoabdou1122 <leoabdou1122@gmail.com>
2023-06-19 14:21:09 -0700

    prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - a program that print all combinations of single-digit numbers.
+ *Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar (i);
+if (i != 57)
+{
+putchar (' ');
+putchar (',');
+}
+}
+putchar ('\n');
+return (0);
+}
