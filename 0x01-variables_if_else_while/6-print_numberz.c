341d708daf29c0221976d4f4ac99474f4051c6ec
leoabdou1122 <leoabdou1122@gmail.com>
2023-06-19 10:47:42 -0700

    print from 0 to 9 using putchar without char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,16 +5,11 @@
 */
 int main(void)
 {
-putchar('0');
-putchar('1');
-putchar('2');
-putchar('3');
-putchar('4');
-putchar('5');
-putchar('6');
-putchar('7');
-putchar('8');
-putchar('9');
+int i;
+for (i = 48; i <= 58; i++)
+{
+putchar(i);
+}
 putchar('\n');
 return (0);
 }
