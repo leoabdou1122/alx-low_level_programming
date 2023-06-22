58abbd991336d96fe488dfac5a3b19f6d673db7d
leoabdou1122 <leoabdou1122@gmail.com>
2023-06-22 14:21:16 -0700

    pritn from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_numbers - a function that print from 0 to 9
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
