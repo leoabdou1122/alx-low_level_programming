00c020792d40cdfcdb6633a7bf03a8600f722118
leoabdou1122 <leoabdou1122@gmail.com>
2023-06-22 10:55:52 -0700

    test the character is uppercase or not

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_isupper - a function that test if upper or not
+ *@n: the character
+ *Return: 0 or 1
+*/
+int _isupper(int n)
+{
+if (n >= 65 && n <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
