f867a95047f060fb0f50288d7e7f919fca5bd2e0
leoabdou1122 <leoabdou1122@gmail.com>
2023-06-22 12:49:39 -0700

    multiple two variable

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *mul - a function that multilple two variable
+ *@a: the fist number
+ *@b: the second number
+ *Return: s
+*/
+int mul(int a, int b)
+{
+s = a * b;
+return (s);
+}
