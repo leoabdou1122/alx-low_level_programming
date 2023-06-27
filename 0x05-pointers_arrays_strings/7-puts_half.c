3d877660bc3a73a101cc01df70e7cf7a8192e9ca
leoabdou1122 <leoabdou1122@gmail.com>
2023-06-27 16:02:09 -0700

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,10 +12,21 @@ int f;
 int s;
 int i;
 f = strlen(str);
+if (f % 2 == 0)
+{
 s = f / 2;
 for (i = s; i < f; i++)
 {
 printf("%c", str[i]);
+}
+}
+else
+{
+s = (f - 1) / 2; 
+for (i = s; i < f; i++)
+{
+printf("%c", str[i]);
+}
 }
 printf("\n");
 }
